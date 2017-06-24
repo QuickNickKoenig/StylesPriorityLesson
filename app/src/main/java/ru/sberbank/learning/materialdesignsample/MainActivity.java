4a77dc1d48f3a26ac47b9875a36a1070fe71f41e
dimasokol.ru <me@dimasokol.ru>
2017-06-24 15:08:42 +0300

    Кастомизация тем

--- app/src/main/java/ru/sberbank/learning/materialdesignsample/MainActivity.java
+++ app/src/main/java/ru/sberbank/learning/materialdesignsample/MainActivity.java
@@ -1,7 +1,10 @@
 package ru.sberbank.learning.materialdesignsample;
 
+import android.support.design.widget.TextInputLayout;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        setSupportActionBar((Toolbar) findViewById(R.id.toolbar));
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        TextInputLayout textInputLayout = (TextInputLayout) findViewById(R.id.text_input2);
+        textInputLayout.setError("Error");
+
+//        AlertDialog dialog = new AlertDialog.Builder(this)
+//                .setTitle("Title")
+//                .setMessage("Message")
+//                .setPositiveButton("OK", null)
+//                .setNeutralButton("Maybe", null)
+//                .setNegativeButton("No", null)
+//                .create();
+//        dialog.show();
     }
 }
